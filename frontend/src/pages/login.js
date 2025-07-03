d3336da8aea0f2e4fdb8208479549d30afd7d684
Jose Mateo Lopez Cifuentes <aprendiz.it@consulting.local>
2025-07-03 14:06:09 -0500

    login

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/img/logo_meridian.png';
+import logo from '../assets/img/logo_meridian_blanco.png';
 import '../assets/css/styles.css';
 
 const Login = () => {
